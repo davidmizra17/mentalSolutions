070715e0ef07878b7f8824898a109524d9d8bcd0
Nicolás Leal <lealnicolas96@gmail.com>
2023-02-14 01:08:37 -0400

    Handle user context

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,14 +1,17 @@
 import { Navbar } from "../../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import { UserContextProvider } from "../../contexts/UserContext";
 
 export function Layout() {
   return (
-    <main>
-      <Navbar />
+    <UserContextProvider>
+      <main>
+        <Navbar />
 
-      <section className="body">
-        <Outlet />
-      </section>
-    </main>
+        <section className="body">
+          <Outlet />
+        </section>
+      </main>
+    </UserContextProvider>
   );
 }
