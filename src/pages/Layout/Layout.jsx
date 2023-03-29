5f1f4fe30b3876ea8a78578988f0418d81795ea1
Andrés Gómez <andres.gomez@correo.unimet.edu.ve>
2023-03-29 14:02:37 -0400

    navbar

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,13 +1,28 @@
 import { Navbar } from "../../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import { UserContextProvider } from "../../contexts/UserContext";
+import logos from "../../assets/mental_solutions.png"
+import styles from "./Layout.module.css";
 
 export function Layout() {
   return (
     <UserContextProvider>
       <main>
-        <Navbar />
-
+      <nav>
+        <input type="checkbox" id="check"></input>
+        <label for="check" className={styles.checkbtn}>
+            <img src="img/barras.png" className={styles.bar}></img>
+        </label>
+        <a href="#" className={styles.enlace}>
+            <img src={logos} alt="" className={styles.logo}></img>
+        </a>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Iniciar Sesion</a></li>
+            <li><a className={styles.active} href="#">Registrarse</a></li>
+        </ul>
+    </nav>
         <section className="body">
           <Outlet />
         </section>
