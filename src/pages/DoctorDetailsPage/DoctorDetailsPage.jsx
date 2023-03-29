c3004f8203fc6946e77f026865c4a02a4894a837
Ernesto Lugo <ernesto.lugo@correo.unimet.edu.ve>
2023-03-29 16:03:02 -0400

    funcion getDocs lista

--- src/pages/DoctorDetailsPage/DoctorDetailsPage.jsx
+++ src/pages/DoctorDetailsPage/DoctorDetailsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../firebase/config";
 import { useParams } from "react-router-dom";
+import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 
 export function DoctorDetailsPage() {
   const [doctorInfo, setDoctorInfo] = useState(null);
@@ -8,18 +9,17 @@ export function DoctorDetailsPage() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const userRef = db.collection("users").doc(id);
-        const userDoc = await userRef.get();
-        setUser(userDoc.data());
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchUser();
-  }, [id]);
+    async function fetchData() {
+      const usersRef = collection(db, "cities");
+      const querySnapshot = await getDocs(collection(db, "users"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        //TODO: aca
+        console.log(doc.id, " => ", doc.data());
+      });
+    }
+    fetchData();
+  }, []);
 
   //   const fetchUser = async () => {
   //     try {
