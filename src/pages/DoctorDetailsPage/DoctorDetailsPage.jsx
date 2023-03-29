a357b5fa1ecb22df86267544785b856ea4d2c6bc
Ernesto Lugo <ernesto.lugo@correo.unimet.edu.ve>
2023-03-29 14:21:20 -0400

    working

--- src/pages/DoctorDetailsPage/DoctorDetailsPage.jsx
+++ src/pages/DoctorDetailsPage/DoctorDetailsPage.jsx
@@ -1,38 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { db } from "../../firebase/config";
+import { useParams } from "react-router-dom";
 
 export function DoctorDetailsPage() {
-  const location = useLocation();
-  const user = location.state.user;
   const [doctorInfo, setDoctorInfo] = useState(null);
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    db.collection("users")
-      .where("name", "==", user.name)
-      .get()
-      .then((querySnapshot) => {
-        if (!querySnapshot.empty) {
-          const userData = querySnapshot.docs[0].data();
-          const { gender, name, email } = userData;
-          setDoctorInfo({ gender, name, email });
-        } else {
-          console.log("No matching documents!");
-        }
-      })
-      .catch((error) => {
-        console.log("Error getting documents:", error);
-      });
-  }, [user.name]);
+    // db.collection("users")
+    //   .where("name", "==", user.name)
+    //   .get()
+    //   .then((querySnapshot) => {
+    //     if (!querySnapshot.empty) {
+    //       const userData = querySnapshot.docs[0].data();
+    //       const { gender, name, email } = userData;
+    //       setDoctorInfo({ gender, name, email });
+    //     } else {
+    //       console.log("No matching documents!");
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.log("Error getting documents:", error);
+    //   });
+
+    fetch(`/api/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
+  }, [id]);
 
   return (
     <div>
-      {doctorInfo ? (
+      {user ? (
         <div>
           <h1>Doctor's information:</h1>
-          <p>Name: {doctorInfo.name}</p>
-          <p>Gender: {doctorInfo.gender}</p>
-          <p>Email: {doctorInfo.email}</p>
+          <p>Name: {user.name}</p>
+          <p>Gender: {user.gender}</p>
+          <p>Email: {user.email}</p>
         </div>
       ) : (
         <p>Loading doctor's information...</p>
